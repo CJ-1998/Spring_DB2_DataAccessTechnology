fcc56e885bfbfdf40a10c799ba9dcabd70f025fe
CJ <choijun0627@konkuk.ac.kr>
2023-09-03 21:14:54 +0900

    <feat> 스프링 DB 2편 프로젝트 section 4
    MyBatis 설정 및 실행
    
    실행하기 위해 MyBatisItemRepository 생성
    이 리포지토리를 사용하기 위해 MyBatisConfig 생성
    ItemService에 @Import를 MyBatisConfig 사용하는 것으로 수정

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,6 +1,7 @@
 package hello.itemservice;
 
 import hello.itemservice.config.JdbcTemplateV3Config;
+import hello.itemservice.config.MyBatisConfig;
 import hello.itemservice.repository.ItemRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
@@ -16,7 +17,8 @@ import javax.sql.DataSource;
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
-@Import(JdbcTemplateV3Config.class)
+//@Import(JdbcTemplateV3Config.class)
+@Import(MyBatisConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
