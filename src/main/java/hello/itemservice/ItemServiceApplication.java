7933277f37976823fb3610bc68cf61cc925498f3
CJ <choijun0627@konkuk.ac.kr>
2023-09-05 23:44:03 +0900

    <feat> 스프링 DB 2편 프로젝트 section 6
    스프링 데이터 JPA 적용
    
    ItemService가 ItemRepository에 의존해서 스프링 데이터 JPA 사용하려면 중간 역할하는 JpaItemRepositoryV2 필요해서 생성
    여기서 ItemRepository 구현하고 SpringDataJpaItemRepository 주입 받아서 스프링 데이터 JPA 사용
    스프링 데이터 JPA 사용하기 위해 SpringDataJpaConfig 생성하고
    ItemServiceApplication의 @Import 수정
    hibernate 버전 버그로 인해 버전 낮추기 위해 build.gradle에 설정 추가

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -3,6 +3,7 @@ package hello.itemservice;
 import hello.itemservice.config.JdbcTemplateV3Config;
 import hello.itemservice.config.JpaConfig;
 import hello.itemservice.config.MyBatisConfig;
+import hello.itemservice.config.SpringDataJpaConfig;
 import hello.itemservice.repository.ItemRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
@@ -20,7 +21,8 @@ import javax.sql.DataSource;
 //@Import(JdbcTemplateV2Config.class)
 //@Import(JdbcTemplateV3Config.class)
 //@Import(MyBatisConfig.class)
-@Import(JpaConfig.class)
+//@Import(JpaConfig.class)
+@Import(SpringDataJpaConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
