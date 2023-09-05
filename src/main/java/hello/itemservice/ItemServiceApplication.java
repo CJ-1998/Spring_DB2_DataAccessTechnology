c711e96f92e6b5aee719a587a887ce15ee42e3de
CJ <choijun0627@konkuk.ac.kr>
2023-09-05 14:58:06 +0900

    <feat> 스프링 DB 2편 프로젝트 section 5
    JPA 적용
    
    JPA 적용 위해 Item 객체와 테이블 매핑하기 위해 Item 클래스에 @Entity,
    @Id, @Column 이런 annotation 추가
    JpaItemRepositoryV1 생성해서 jpa 사용해서 쉽게 쿼리 날리고 할 수 있음
    JpaItemRepositoryV1 사용하기 위해 JpaConfig 생성 및
    ItemServiceApplication에 @Import 수정

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,6 +1,7 @@
 package hello.itemservice;
 
 import hello.itemservice.config.JdbcTemplateV3Config;
+import hello.itemservice.config.JpaConfig;
 import hello.itemservice.config.MyBatisConfig;
 import hello.itemservice.repository.ItemRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -18,7 +19,8 @@ import javax.sql.DataSource;
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
 //@Import(JdbcTemplateV3Config.class)
-@Import(MyBatisConfig.class)
+//@Import(MyBatisConfig.class)
+@Import(JpaConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
